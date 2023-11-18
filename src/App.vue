36aa875f1f2732a136839e699a7a81c22656cc85
Cheng Feng <zack.chengf@gmail.com>
2023-11-18 11:58:11 +0800

    学习模版引用

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,13 @@
 <script>
 export default {
-  data() {
-    return {
-      question: '',
-      answer: 'Questions usually contain a question mark. ;-)'
-    }
-  },
-  watch: {
-    question(newQuestion, oldQuestion) {
-      if (newQuestion.includes('?')) {
-        this.getAnswer()
-      }
-    }
-  },
-  methods: {
-    async getAnswer() {
-      this.answer = 'Thinking...'
-      try {
-        const res = await fetch('https://yesno.wtf/api')
-        this.answer = (await res.json()).answer
-      } catch (error) {
-        this.answer = 'Error! Could not reach the API. ' + error
-      }
-    }
+  mounted() {
+    this.$refs.input.focus()
   }
 }
 </script>
 
 <template>
-  <p>
-    Ask a yes/no question:
-    <input v-model="question" />
-  </p>
-  <p>{{ answer }}</p>
+  <input ref="input">
 </template>
 
 <!-- <script setup>
